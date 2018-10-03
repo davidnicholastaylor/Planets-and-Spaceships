0688906a545ed5f8b1c53b25fa06fa5ff358baf9
davidnicholastaylor <dntaylor14@gmail.com>
2018-10-03 11:57:48 -0500

    finished first half

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> planetListTwo = new List<string>(){"Neptune", "Uranus"};
+            
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.AddRange(planetListTwo);
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.RemoveAt(8);
+
+            planetList.ForEach(planet => Console.WriteLine(planet));
+        }
+    }
+}
